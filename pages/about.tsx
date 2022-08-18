f6b91495ef315a944d1e6b52b645149c7480b5be
TojoFabrice <43203071+TojoFabrice@users.noreply.github.com>
2022-08-18 16:58:06 +0300

    Add files via upload

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const about: NextPage = () => {
+    return (
+        <p className='text-red-500 font-bold text-lg'>about page</p>
+    )
+}
+
+export default about
